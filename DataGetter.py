ca78a6d0a6197cd415c8e1f07792d432acc2a883
ehutmire <ehutmire@gmail.com>
2022-09-20 16:47:05 -0400

    Update DataGetter.py
    added TwitterDataGetter class, which implements DataGetter class

--- DataGetter.py
+++ DataGetter.py
@@ -4,3 +4,10 @@ class DataGetter(ABC):
     @abstractmethod
     def get_Data(usrname):
         pass
+
+
+class TwitterDataGetter(DataGetter):
+    def get_Data(usrname, client):
+        query = 'from:' + usrname + ' -is:retweet'
+        tweets = client.search_recent_tweets(query=query, tweet_fields=['created_at'], max_results=10)
+        return tweets
