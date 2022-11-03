f40ccb9be5580e5619efa14f07ad69b4521dcf81
qiujerry <qiujer5687@gmail.com>
2022-11-02 23:16:56 -0400

    elastic data entry attempt

--- elastic_setup.py
+++ elastic_setup.py
@@ -1,7 +1,25 @@
 from elasticsearch import Elasticsearch
 import config
 
+import profile
+import os
+import json
+
 ELASTIC_PASSWORD = config.elastic_pass
 
-es = Elasticsearch('https://localhost:9200')
+es = Elasticsearch(hosts = 'https://localhost:9200' , basic_auth=["elastic", ELASTIC_PASSWORD])
 
+
+
+
+fileName = "testDataBoogaloo.json"
+Profiles = {}
+if os.path.getsize(fileName) != 0:
+    with open(fileName) as infile:
+        Profiles = json.load(infile)
+
+# for prof in Profiles:
+#     Profiles[prof] = profile.from_json(Profiles[prof])
+
+resp = es.index(index="test-index", id="hasley-test", document=json.dumps(Profiles["halsey"]))
+print(resp['result'])
